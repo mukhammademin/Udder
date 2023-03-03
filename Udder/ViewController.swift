13a807b61e135f117f75b0108c839942c24b38c8
Mukhammademin <mukhammademin19@icloud.com>
2023-03-03 13:01:32 -0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Mukhammademin Eminov on 03/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
